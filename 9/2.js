6eab1796d6cdbfb55f689f9a8fd5ea531cac4705
BrianBakkala <45982065+BrianBakkala@users.noreply.github.com>
2025-12-09 16:00:05 -0500

    Extend rectangle setup

--- 9/2.js
+++ 9/2.js
@@ -1,8 +1,8 @@
-const vertices = [];
+const shape = [];
 let maxX = 0;
 let maxY = 0;
 
-readFile(9, 1, function (r)
+readFile(9, 0, function (r)
 {
 
     for (let unitIndex in r)
@@ -10,7 +10,7 @@ readFile(9, 1, function (r)
         const unit = r[unitIndex];
         const [x, y] = (unit.split(",").map(Number));
 
-        vertices.push({ x, y });
+        shape.push([x, y]);
         if (x > maxX)
         {
             maxX = x;
@@ -21,8 +21,55 @@ readFile(9, 1, function (r)
         }
     }
 
-    console.log(vertices);
-    console.log(geometric);
+    //close polygon
+    console.log(geometric.polygonArea(shape));
+    shape.push(r[0].split(",").map(Number));
+    console.log(geometric.polygonArea(shape));
+
+
+    for (let ind1 in shape)
+    {
+        for (let ind2 in shape)
+        {
+            if (ind1 !== ind2
+                && shape[ind1][0] != shape[ind2][0]
+                && shape[ind1][1] != shape[ind2][1]
+
+
+
+                && shape[ind1][0] != shape[ind2][1]
+                && shape[ind1][1] != shape[ind2][0]
+            )
+            {
+                const point1 = shape[ind1];
+                const point2 = shape[ind2];
+
+                // console.log([point1, point2]);
+                console.log(extendRectangle(point1, point2));
+                break;
+            }
+        }
+        break;
+
+    }
+
+    // console.log(shape);
+    // console.log(geometric.polygonBounds(shape));
 
     return;
 });
+
+
+function extendRectangle(point1, point2)
+{
+    const cornerPoints = [point1, point2];
+
+    const minX = Math.min(point1[0], point2[0]);
+    const minY = Math.min(point1[1], point2[1]);
+
+    console.log(cornerPoints);
+
+
+
+
+}
