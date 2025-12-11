7f0c2c30965bb0b090465844bb7e2d59ebdf1f9c
BrianBakkala <45982065+BrianBakkala@users.noreply.github.com>
2025-12-11 16:02:01 -0500

    Solution to 11.1

---
+++ 11/1.js
@@ -0,0 +1,43 @@
+readFile(11, 1, function (r)
+{
+    const network = {};
+    const paths = [];
+    let pathCount = 0;
+
+    for (let unit of r)
+    {
+        const start = unit.split(":")[0].trim();
+        const ends = unit.split(":")[1].trim().split(" ");
+
+        network[start] = ends;
+    }
+
+
+    buildPath([], 'you');
+
+    function buildPath(pathSoFar, node) 
+    {
+        if (pathSoFar.includes(node))
+        {
+            return;
+        }
+        else if (node === 'out')
+        {
+            // paths.push([...pathSoFar, node]);
+            pathCount += 1;
+            return;
+        }
+
+        const nextNodes = network[node];
+        pathSoFar.push(node);
+        for (const next of nextNodes)
+        {
+            buildPath([...pathSoFar], next);
+        }
+    }
+
+    // console.log(paths);
+    console.log(pathCount);
+
+
+});
---
+++ 11/2.js
@@ -0,0 +1,10 @@
+readFile(0, 0, function (r)
+{
+
+    for (let unit of r)
+    {
+        console.log(unit);
+    }
+
+
+});
